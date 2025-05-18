4d1b3635c8dfed9726da3c8a839319f53d8b12c1
luisafalquetoz <luisa_fzandonade@hotmail.com>
2025-05-18 17:00:37 -0300

    feat: add authentication with clerk

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,8 +1,15 @@
+import { SignInButton } from '@clerk/nextjs';
+import { auth } from '@clerk/nextjs/server';
 import { LogInIcon } from 'lucide-react';
 import Image from 'next/image';
+import { redirect } from 'next/navigation';
 import { Button } from '../_components/ui/button';
 
-const LoginPage = () => {
+const LoginPage = async () => {
+	const { userId } = await auth();
+	if (userId) {
+		redirect('/');
+	}
 	return (
 		<div className="grid h-full grid-cols-2">
 			<div className="mx-auto flex h-full max-w-[550px] flex-col justify-center p-8">
@@ -19,10 +26,12 @@ const LoginPage = () => {
 					monitorar suas movimentações, e oferecer insights personalizados,
 					facilitando o controle do seu orçamento.
 				</p>
-				<Button variant="outline">
-					<LogInIcon className="mr-2" />
-					Fazer login ou criar conta
-				</Button>
+				<SignInButton>
+					<Button variant="outline">
+						<LogInIcon className="mr-2" />
+						Fazer login ou criar conta
+					</Button>
+				</SignInButton>
 			</div>
 
 			<div className="relative h-full w-full">
