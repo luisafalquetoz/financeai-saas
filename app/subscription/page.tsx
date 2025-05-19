63dce271013362a627777ec7c1a71ce5577a30a1
luisafalquetoz <luisa_fzandonade@hotmail.com>
2025-05-19 16:17:21 -0300

    feat: ensure transactions and subscription pages are protected

--- app/subscription/page.tsx
+++ app/subscription/page.tsx
@@ -1,6 +1,13 @@
+import { auth } from '@clerk/nextjs/server';
+import { redirect } from 'next/navigation';
 import Navbar from '../_components/navbar';
 
-const SubscriptionPage = () => {
+const SubscriptionPage = async () => {
+	const { userId } = await auth();
+	if (!userId) {
+		redirect('/login');
+	}
+
 	return (
 		<>
 			<Navbar />
--- app/transactions/page.tsx
+++ app/transactions/page.tsx
@@ -1,3 +1,5 @@
+import { auth } from '@clerk/nextjs/server';
+import { redirect } from 'next/navigation';
 import AddTransactionButton from '../_components/add-transaction-button';
 import Navbar from '../_components/navbar';
 import { DataTable } from '../_components/ui/data-table';
@@ -5,7 +7,17 @@ import { db } from '../_lib/prisma';
 import { transactionColumns } from './_columns';
 
 const TransactionsPage = async () => {
-	const transactions = await db.transaction.findMany({});
+	const { userId } = await auth();
+	if (!userId) {
+		redirect('/login');
+	}
+
+	const transactions = await db.transaction.findMany({
+		where: {
+			userId,
+		},
+	});
+
 	return (
 		<>
 			<Navbar />
